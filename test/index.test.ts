bbf4e6ac590b0da61de03673d9f75758586187db
agehama <agehama1@gmail.com>
2020-10-11 17:59:50 +0900

    fix

--- test/index.test.ts
+++ test/index.test.ts
@@ -8,6 +8,6 @@ test("add", () =>
 
 test("gles", () =>
 {
-    const gl = nodeGles.binding.createWebGLRenderingContext();
+    const gl = nodeGles.createWebGLRenderingContext();
     expect(gl).not.toBeNull();
 });
