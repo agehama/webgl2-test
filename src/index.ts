b8eff511c64281531605221b3d4d8a27ddb75fb1
agehama <agehama1@gmail.com>
2020-10-11 21:55:11 +0900

    link shader

--- src/index.ts
+++ src/index.ts
@@ -7,26 +7,35 @@ export const func = ():boolean =>
     const gl = nodeGles.createWebGLRenderingContext({});
 
     const vs = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vs,
+    {
+        gl.shaderSource(vs,
 `#version 300 es
-
-uniform mat4 u_worldViewProjection;
-in vec4 position;
 
 void main()
 {
-    gl_Position = u_worldViewProjection * position;
-}`
+    vec3[6] vertices = vec3[](
+        vec3(-1.0, -1.0, 0),
+        vec3(+1.0, -1.0, 0),
+        vec3(-1.0, +1.0, 0),
+        vec3(-1.0, +1.0, 0),
+        vec3(+1.0, -1.0, 0),
+        vec3(+1.0, +1.0, 0)
     );
-    gl.compileShader(vs);
-    if(!gl.getShaderParameter(vs, gl.COMPILE_STATUS))
-    {
-        console.log(gl.getShaderInfoLog(vs));
-        return false;
+
+    gl_Position = vec4(vertices[gl_VertexID], 1);
+}`
+        );
+        gl.compileShader(vs);
+        if(!gl.getShaderParameter(vs, gl.COMPILE_STATUS))
+        {
+            console.log(gl.getShaderInfoLog(vs));
+            return false;
+        }
     }
 
     const fs = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fs, 
+    {
+        gl.shaderSource(fs, 
 `#version 300 es
 precision mediump float;
 
@@ -36,27 +45,33 @@ void main()
 {
     outColor = vec4(1, 1, 0, 1);
 }`
-    );
-    gl.compileShader(fs);
-    if(!gl.getShaderParameter(fs, gl.COMPILE_STATUS))
-    {
-        console.log(gl.getShaderInfoLog(fs));
-        return false;
+        );
+        gl.compileShader(fs);
+        if(!gl.getShaderParameter(fs, gl.COMPILE_STATUS))
+        {
+            console.log(gl.getShaderInfoLog(fs));
+            return false;
+        }
     }
 
     const program = gl.createProgram();
-    gl.attachShader(program, vs);
-    gl.attachShader(program, fs);
-
-    gl.linkProgram();
-
-    if(!gl.getProgramParameter(program, gl.LINK_STATUS))
     {
-        console.log(gl.getShaderInfoLog(program));
-        return false;
+        gl.attachShader(program, vs);
+        gl.attachShader(program, fs);
+
+        gl.linkProgram(program);
+
+        if(!gl.getProgramParameter(program, gl.LINK_STATUS))
+        {
+            console.log(gl.getShaderInfoLog(program));
+            return false;
+        }
+
+        gl.useProgram(program);
     }
 
-
+    //gl.drawArrays(gl.TRIANGLES, 0, 6);
+    //gl.flush();
 
     return true;
 }
