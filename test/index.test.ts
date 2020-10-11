27d88bac1a18941b1b3ba376fff5f6dd916776ef
agehama <agehama1@gmail.com>
2020-10-11 17:54:47 +0900

    add node-gles

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,13 @@
 import {add} from "../src/index";
+const nodeGles = require('node-gles');
 
 test("add", () =>
 {
     expect(add(1, 1)).toBe(2);
+});
+
+test("gles", () =>
+{
+    const gl = nodeGles.binding.createWebGLRenderingContext();
+    expect(gl).not.toBeNull();
 });
