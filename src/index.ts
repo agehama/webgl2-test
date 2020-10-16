8ebb1a3bbf2c7c507edd6b52afb21ea8a07379e6
agehama <agehama1@gmail.com>
2020-10-16 22:35:16 +0900

    revert

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,11 @@
-// const main = document.getElementById("main");
-// if(main != null)
-// {
-//     main.innerHTML = `<h1>test1</h1>`;
-// }
-
 export const add = (a: number, b: number): number => a + b;
 
-export function getString(): string[]
-{
-    return [`test1 test2 ab`, `abc`, `test`];
-}
-
-export function webgl2TestFunc(gl: WebGL2RenderingContext): string
+export const func = (gl: WebGLRenderingContext):boolean =>
 {
     const vs = gl.createShader(gl.VERTEX_SHADER) as WebGLShader;
     {
         gl.shaderSource(vs,
 `#version 300 es
-
 void main()
 {
     vec3[6] vertices = vec3[](
@@ -28,7 +16,6 @@ void main()
         vec3(+1.0, -1.0, 0),
         vec3(+1.0, +1.0, 0)
     );
-
     gl_Position = vec4(vertices[gl_VertexID], 1);
 }`
         );
@@ -36,7 +23,7 @@ void main()
         if(!gl.getShaderParameter(vs, gl.COMPILE_STATUS))
         {
             console.log(gl.getShaderInfoLog(vs));
-            return `error 1`;
+            return false;
         }
     }
 
@@ -45,24 +32,17 @@ void main()
         gl.shaderSource(fs, 
 `#version 300 es
 precision mediump float;
-
-uniform vec2 u_resolution;
-out uvec4 outColor[4];
-
+out vec4 outColor;
 void main()
 {
-    uvec2 xy = uvec2(gl_FragCoord.xy);
-    outColor[0] = uvec4(xy + uvec2( 0,  0), 0u, 255u);
-    outColor[1] = uvec4(xy + uvec2(10, 10), 0u, 255u);
-    outColor[2] = uvec4(xy + uvec2(20, 20), 0u, 255u);
-    outColor[3] = uvec4(xy + uvec2(30, 30), 0u, 255u);
+    outColor = vec4(0, 1, 0.5, 1);
 }`
         );
         gl.compileShader(fs);
         if(!gl.getShaderParameter(fs, gl.COMPILE_STATUS))
         {
             console.log(gl.getShaderInfoLog(fs));
-            return `error 2`;
+            return false;
         }
     }
 
@@ -76,53 +56,31 @@ void main()
         if(!gl.getProgramParameter(program, gl.LINK_STATUS))
         {
             console.log(gl.getShaderInfoLog(program));
-            return `error 3`;
+            return false;
         }
 
         gl.useProgram(program);
     }
 
-    return `succeeded`;
-    /*
     const width = 2;
     const height = 2;
-    const depth = 4;
     const texture = gl.createTexture();
-    gl.bindTexture(gl.TEXTURE_3D, texture);
-    gl.texImage3D(gl.TEXTURE_3D, 0, gl.RGBA8UI, width, height, depth, 0, gl.RGBA_INTEGER, gl.UNSIGNED_BYTE, null);
-
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+    
     const fb = gl.createFramebuffer();
     gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
-    for(let z = 0; z < 4; z++)
-    {
-        gl.framebufferTextureLayer(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0 + z, texture, 0, z);
-    }
-    gl.drawBuffers([gl.COLOR_ATTACHMENT0, gl.COLOR_ATTACHMENT1, gl.COLOR_ATTACHMENT2, gl.COLOR_ATTACHMENT3]);
-
-    const locResolution = gl.getUniformLocation(program, 'u_resolution');
-    gl.uniform2f(locResolution, width, height);
+    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
     
-    gl.clearBufferuiv(gl.COLOR, 0, [0,0,0,0]);
-    gl.disable(gl.BLEND);
-    gl.disable(gl.DEPTH_TEST);
     gl.viewport(0, 0, width, height);
     gl.drawArrays(gl.TRIANGLES, 0, 6);
     gl.flush();
 
-    let results = [];
     const pixels = new Uint8Array(4*width*height);
-    for(let z = 0; z < 4; z++)
-    {
-        gl.readBuffer(gl.COLOR_ATTACHMENT0 + z);
-        gl.readPixels(0, 0, width, height, gl.RGBA_INTEGER, gl.UNSIGNED_BYTE, pixels);
-        results.push(pixels.slice());
-    }
+    gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
 
-    //return JSON.stringify(results);
-    return results;*/
-}
+    console.log(pixels);
 
-export function getString2(): string
-{
-    return `test6 test7 test8`;
+    return true;
 }
