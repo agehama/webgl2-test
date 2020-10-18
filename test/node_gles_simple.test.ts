3c390694100ffae9120082a38ce9c200ba2b2eb1
agehama <agehama1@gmail.com>
2020-10-18 14:54:37 +0900

    add test

--- test/node_gles_simple.test.ts
+++ test/node_gles_simple.test.ts
@@ -1,9 +1,23 @@
 const nodeGles = require('node-gles');
 import {webglSimple} from "../src/index";
 
-test("simple (node-gles)", () =>
+test("simple (node-gles - ES 2.0)", () =>
 {
-    const gl = nodeGles.createWebGLRenderingContext();
+    const gl = nodeGles.createWebGLRenderingContext({
+        client_major_es_version: 2,
+        client_minor_es_version: 0,
+    });
+    const result = webglSimple(gl);
+    console.log(result);
+    expect(result).not.toBe([]);
+});
+
+test("simple (node-gles - ES 3.0)", () =>
+{
+    const gl = nodeGles.createWebGLRenderingContext({
+        client_major_es_version: 2,
+        client_minor_es_version: 0,
+    });
     const result = webglSimple(gl);
     console.log(result);
     expect(result).not.toBe([]);
